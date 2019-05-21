d666c4c15c50b27cf5ee29cfa64c15484fc4feab
Instituto Federal de Educação Ciência e Tecnologia do Tocantins <vinnicyus@pso-labresi-09.ifto.local>
2019-05-21 19:48:32 -0300

    test porta 433

--- demandai_site/urls.py
+++ demandai_site/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about-portifolio/<str:action>/<int:id>', views.about_portifolio, name='about_portifolio'),
     path('demandar/<str:action>/<int:id>', views.demandarSelected, name='demanda_selecionada'),
     path('demandar', views.demandar, name='demandar'),
+
 ]
