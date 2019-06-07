aad72185d2c4d0969bc8fbf239a7a08d2ba75815
wylliansales <wylliansalles@gmail.com>
2019-06-07 17:04:00 -0300

    correção na resolução do site

--- projeto_demandai/settings.py
+++ projeto_demandai/settings.py
@@ -81,21 +81,21 @@ AUTH_USER_MODEL = 'demandai_administrador.Profile'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'NAME': 'demandai',
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'PASSWORD': ''
-    },
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
+#         'NAME': 'demandai',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'USER': 'root',
+#         'PASSWORD': ''
+#     },
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
